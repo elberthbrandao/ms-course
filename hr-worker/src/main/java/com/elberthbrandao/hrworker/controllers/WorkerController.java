aa82fcf7cfa24a101c9d49fcaecb127fa3cd3728
elberth.brandao <zelltr@hotmail.com>
2021-07-29 13:35:27 -0300

    db config server

--- hr-worker/src/main/java/com/elberthbrandao/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/elberthbrandao/hrworker/controllers/WorkerController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -23,9 +22,6 @@ import com.elberthbrandao.hrworker.repositories.WorkerRepository;
 public class WorkerController {
 
 	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
-	
-	@Value("${test.config}")
-	private String testConfig;
 
 	@Autowired
 	private Environment env;
@@ -35,7 +31,7 @@ public class WorkerController {
 
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs() {
-		logger.info("CONFIG = " + testConfig);
+//		logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
 	}
 	
@@ -48,12 +44,12 @@ public class WorkerController {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			Thread.sleep(3000L);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 
 		logger.info("Port: " + env.getProperty("local.server.port"));
 
